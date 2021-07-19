6b6e4658702831d094c64df41395cff1b260a14b
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-07-19 19:59:04 -0300

    Avaliação dos resultados, através da comparação entre as coordenadas do geocoding e o setor censitário correspondente

---
+++ R/02_evaluate_results.R
@@ -0,0 +1,175 @@
+# description -------------------------------------------------------------
+
+# Este script faz o download da base de dados do CNEFE / IBGE, referente ao
+# censo de 2010
+
+# setup -------------------------------------------------------------------
+
+source('R/setup.R')
+source('R/munis_df.R')
+
+# list of Brazilian municipalities
+munis_all_df <- geobr::lookup_muni("all") %>% setDT()
+munis_df[munis_all_df, on = .(code_muni), name_muni_pt := i.name_muni]
+
+
+streetmap_geocoded_sf <- st_read("../../data/geocode/streetmap_eval/cnefe_sample_streetmap_Geocoded.shp") %>%
+  mutate(USER_code_ = as.character(USER_code_))
+streetmap_sample_df <- read_csv("../../data/geocode/streetmap_eval/cnefe_sample_streetmap.csv") %>%
+  mutate(code_tract = as.character(code_tract))
+
+# functions ---------------------------------------------------------------
+# muni <- 2304400 # Fortaleza
+# muni <- 1302603 # Manaus
+# muni <- 3550308 # São Paulo
+# muni <- 3509502 # Campinas
+
+validate_geocoding <- function(muni) {
+  # extract urban area name and uf from data
+  muni_uf <- unique(subset(munis_df, code_muni == muni)$abrev_estado)
+  muni_name <- unique(subset(munis_df, code_muni == muni)$name_muni_pt)
+
+  message(sprintf("Working on city %s / %s", muni_name, muni_uf))
+
+  # prepare geocoded sample
+  sample_df <- streetmap_sample_df %>% filter(city == muni_name) %>% setDT()
+  streetmap_sf <- streetmap_geocoded_sf %>% filter(USER_city == muni_name) %>%
+    st_set_geometry(NULL) %>% setDT()
+
+  sample_df[streetmap_sf, on = .(cnefe_id == USER_cnefe),
+            `:=`(lat = i.Y, lon = i.X,
+                 geocode_status = i.Status,
+                 geocode_score = i.Score,
+                 geocode_match = i.Match_type,
+                 geocode_type = i.Addr_type,
+                 geocode_rank = i.Rank)]
+
+  sample_sf <- st_as_sf(sample_df, coords = c("lon", "lat"), crs = 4326)
+
+  # load census tracts
+  census_sf <- geobr::read_census_tract(code_tract = muni, simplified = FALSE) %>%
+    st_transform(crs = 4326)
+
+  # order census tracts according to sample addresses
+  code_tracts <- sample_sf$code_tract
+  ct_order <- lapply(code_tracts, function(ct) { which(census_sf$code_tract == ct)  }) %>% unlist()
+  census_ordered_sf <- census_sf %>% slice(ct_order)
+  census_ordered_sf <- st_make_valid(census_ordered_sf)
+
+  # calculate distance between geocoded address and target census tract
+  geo_distances <- st_distance(sample_sf, census_ordered_sf, by_element = TRUE)
+  sample_sf$geocode_distance <- as.numeric(geo_distances)
+
+  return(sample_sf)
+}
+
+# apply functions ---------------------------------------------------------
+
+spo_sf <- validate_geocoding(3550308)
+spo_sf %>% filter(geocode_distance < 1000) %>% mapview(zcol="geocode_distance")
+
+codes <- unique(munis_df$code_muni)
+validated_sample_sf <- map_df(codes, validate_geocoding)
+
+validated_sample_sf <- validated_sample_sf %>%
+  mutate(geocode_result = case_when(geocode_distance <= 0 ~ "VALID",
+                                    geocode_distance <= 350 ~ "GOOD",
+                                    geocode_distance <= 1000 ~ "AVERAGE",
+                                    TRUE ~ "BAD"))
+
+validated_sample_sf %>% st_write("../../data/geocode/streetmap_eval/validated_sample.gpkg")
+validated_sample_sf %>%
+  st_set_geometry(NULL) %>%
+  write_csv("../../data/geocode/streetmap_eval/validated_sample.csv")
+
+validated_count_df <- validated_sample_sf %>%
+  st_set_geometry(NULL) %>%
+  count(city, geocode_result)
+
+city_order <- validated_count_df %>%
+  pivot_wider(names_from = geocode_result, values_from = n) %>%
+  ungroup() %>%
+  arrange(VALID, GOOD, AVERAGE, BAD)
+
+validated_count_df %>%
+  mutate(city = factor(city, levels = city_order$city),
+         geocode_result = factor(geocode_result,
+                                 levels = c("VALID", "GOOD", "AVERAGE", "BAD"))) %>%
+  mutate(geocode_result = fct_rev(geocode_result)) %>%
+  ggplot() + geom_col(aes(x = city, y = n, fill = geocode_result)) +
+  coord_flip() +
+  scale_fill_brewer(palette = "Set1", direction = 1) +
+  scale_y_continuous(breaks = seq(0, 1000, 100), minor_breaks = seq(0, 1000, 50)) +
+  labs(title = "Resultado geral do geocoding, por município",
+       x = NULL, y = NULL, fill = NULL,
+       caption = "VALID: dentro do setor censitário\nGOOD: até 350m do setor censitário\nAVERAGE: até 1km do setor censitário\nBAD: acima de 1km do setor censitário"
+       )
+
+validated_sample_sf %>%
+  st_set_geometry(NULL) %>%
+  count(geocode_status, geocode_result) %>%
+  group_by(geocode_status) %>%
+  mutate(p = n / sum(n)) %>%
+  mutate(geocode_status = factor(geocode_status,
+                                 levels = c("M", "T", "U"),
+                                 labels = c("MATCH", "TIE", "UNMATCH"))) %>%
+  ggplot() + geom_col(aes(x = geocode_status, y = p, fill = geocode_result)) +
+  coord_flip() +
+  scale_fill_brewer(palette = "Set1", direction = 1) +
+  scale_y_percent() +
+  # scale_y_continuous(breaks = seq(0, 1000, 100), minor_breaks = seq(0, 1000, 50)) +
+  labs(title = "Resultado geral do geocoding, por STATUS",
+       x = NULL, y = NULL, fill = NULL,
+       caption = "VALID: dentro do setor censitário\nGOOD: até 350m do setor censitário\nAVERAGE: até 1km do setor censitário\nBAD: acima de 1km do setor censitário"
+  )
+
+
+geocode_type_levels <- c("PointAddress", "StreetAddress", "StreetAddressExt", "StreetName",
+                         "PostalExt", "PostalLoc", "Postal", "DistanceMarker")
+
+validated_sample_sf %>%
+  st_set_geometry(NULL) %>%
+  filter(geocode_status != "U") %>%
+  count(geocode_status, geocode_type, geocode_result) %>%
+  group_by(geocode_status, geocode_type) %>%
+  mutate(p = n / sum(n)) %>%
+  mutate(geocode_type = factor(geocode_type, levels = geocode_type_levels)) %>%
+  mutate(geocode_type = fct_rev(geocode_type)) %>%
+  mutate(geocode_status = factor(geocode_status,
+                                 levels = c("M", "T", "U"),
+                                 labels = c("MATCH", "TIE", "UNMATCH"))) %>%
+  ggplot() + geom_col(aes(x = geocode_type, y = p, fill = geocode_result)) +
+  coord_flip() +
+  scale_fill_brewer(palette = "Set1", direction = 1) +
+  scale_y_percent() +
+  # scale_y_continuous(breaks = seq(0, 1000, 100), minor_breaks = seq(0, 1000, 50)) +
+  labs(title = "Resultado geral do geocoding, por ADDR_TYPE",
+       x = NULL, y = NULL, fill = NULL,
+       caption = "VALID: dentro do setor censitário\nGOOD: até 350m do setor censitário\nAVERAGE: até 1km do setor censitário\nBAD: acima de 1km do setor censitário"
+  ) +
+  facet_wrap(~geocode_status, scales = "free", ncol = 1)
+
+validated_sample_sf %>%
+  st_set_geometry(NULL) %>%
+  filter(geocode_status == "M") %>%
+  mutate(geocode_type = factor(geocode_type, levels = geocode_type_levels),
+         geocode_result = factor(geocode_result,
+                                 levels = c("VALID", "GOOD", "AVERAGE", "BAD"))) %>%
+  mutate(geocode_type = fct_rev(geocode_type)) %>%
+  ggplot() + geom_boxplot(aes(x=geocode_type, y = geocode_score, fill = geocode_result)) +
+  geom_hline(yintercept = c(90, 95)) +
+  coord_flip() +
+  scale_fill_brewer(palette = "Set1", direction = 1) +
+  # scale_y_percent() +
+  scale_y_continuous(breaks = seq(0, 100, 5)) +
+  labs(title = "Resultado do geocoding SCORE, por ADDR_TYPE",
+       x = NULL, y = NULL, fill = NULL,
+       caption = "VALID: dentro do setor censitário\nGOOD: até 350m do setor censitário\nAVERAGE: até 1km do setor censitário\nBAD: acima de 1km do setor censitário"
+  ) +
+  facet_wrap(~geocode_result, nrow = 1)
+
+
+
+
+
+
