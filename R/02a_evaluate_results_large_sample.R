95a98e4982f410e7dfebbd0c7fc28e5820ebac35
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-07-30 11:32:54 -0300

    Adicionada a avaliação do Google Maps

---
+++ R/02a_evaluate_results_large_sample.R
@@ -0,0 +1,97 @@
+# description -------------------------------------------------------------
+
+# Este script faz o download da base de dados do CNEFE / IBGE, referente ao
+# censo de 2010
+
+# setup -------------------------------------------------------------------
+
+source('R/setup.R')
+source('R/munis_df.R')
+
+# list of Brazilian municipalities
+munis_all_df <- geobr::lookup_muni("all") %>% setDT()
+munis_df[munis_all_df, on = .(code_muni), name_muni_pt := i.name_muni]
+
+# File names
+
+## geocode completo
+streetmap_geocoded_sf <- st_read("../../data/geocode/streetmap_eval/cnefe_large_sample_streetmap_geocode/cnefe_large_sample_streetmap_Geocoded_completo_v2.shp") %>%
+  mutate(USER_code_ = as.character(USER_code_))
+output_sf <- "../../data/geocode/streetmap_eval/validated_large_sample_completo.gpkg"
+output_csv <- "../../data/geocode/streetmap_eval/validated_large_sample_completo.csv"
+
+
+# Load sample addresses
+streetmap_sample_df <- read_csv("../../data/geocode/streetmap_eval/cnefe_large_sample_streetmap.csv") %>%
+  mutate(code_tract = as.character(code_tract))
+
+# functions ---------------------------------------------------------------
+# muni <- 2304400 # Fortaleza
+# muni <- 1302603 # Manaus
+# muni <- 3550308 # São Paulo
+# muni <- 3509502 # Campinas
+
+validate_geocoding <- function(muni) {
+  # extract urban area name and uf from data
+  muni_uf <- unique(subset(munis_df, code_muni == muni)$abrev_estado)
+  muni_name <- unique(subset(munis_df, code_muni == muni)$name_muni_pt)
+
+  message(sprintf("Working on city %s / %s", muni_name, muni_uf))
+
+  # prepare geocoded sample
+  sample_df <- streetmap_sample_df %>% filter(city == muni_name) %>% setDT()
+  streetmap_sf <- streetmap_geocoded_sf %>% filter(USER_city == muni_name) %>%
+    st_set_geometry(NULL) %>% setDT()
+
+  sample_df[streetmap_sf, on = .(cnefe_id == USER_cnefe),
+            `:=`(lat = i.Y, lon = i.X,
+                 geocode_status = i.Status,
+                 geocode_score = i.Score,
+                 geocode_match = i.Match_type,
+                 geocode_type = i.Addr_type,
+                 geocode_rank = i.Rank)]
+
+  sample_sf <- st_as_sf(sample_df, coords = c("lon", "lat"), crs = 4326)
+
+  # load census tracts
+  census_sf <- geobr::read_census_tract(code_tract = muni, simplified = FALSE) %>%
+    st_transform(crs = 4326)
+
+  # order census tracts according to sample addresses
+  code_tracts <- sample_sf$code_tract
+  ct_order <- lapply(code_tracts, function(ct) { which(census_sf$code_tract == ct)  }) %>% unlist()
+  census_ordered_sf <- census_sf %>% slice(ct_order)
+  census_ordered_sf <- st_make_valid(census_ordered_sf)
+
+  # calculate distance between geocoded address and target census tract
+  geo_distances <- st_distance(sample_sf, census_ordered_sf, by_element = TRUE)
+  sample_sf$geocode_distance <- as.numeric(geo_distances)
+
+  return(sample_sf)
+}
+
+# apply functions ---------------------------------------------------------
+
+# spo_sf <- validate_geocoding(3550308)
+# spo_sf %>% filter(geocode_distance < 1000) %>% mapview(zcol="geocode_distance")
+
+codes <- unique(munis_df$code_muni)
+validated_sample_sf <- map_df(codes, validate_geocoding)
+
+validated_sample_sf <- validated_sample_sf %>%
+  mutate(geocode_result = case_when(geocode_distance <= 0 ~ "VALID",
+                                    geocode_distance <= 350 ~ "GOOD",
+                                    geocode_distance <= 1000 ~ "AVERAGE",
+                                    TRUE ~ "BAD"))
+
+
+validated_sample_sf %>% st_write(output_sf)
+validated_sample_sf %>%
+  st_set_geometry(NULL) %>%
+  write_csv(output_csv)
+
+
+
+
+
+
--- R/04_compare_geolocators.R
+++ R/04_compare_geolocators.R
@@ -25,32 +25,45 @@ validated_galileo_df <- read_csv("../../data/geocode/streetmap_eval/validated_sa
   select(cnefe_id:landuse_description, geocode_distance, geocode_result) %>%
   mutate(locator = "galileo")
 
+validated_gmaps_df <- read_csv("../../data/geocode/streetmap_eval/validated_sample_gmaps.csv") %>%
+  select(cnefe_id:landuse_description, geocode_distance, geocode_result) %>%
+  mutate(locator = "google_maps")
+
 validated_sample_df <- rbind(validated_address_df,
                              validated_address_zip_df,
                              validated_complete_df,
-                             validated_galileo_df) %>%
+                             validated_galileo_df,
+                             validated_gmaps_df) %>%
   mutate(code_tract = as.character(code_tract))
 
 # By Locator and City
 
 validated_count_by_city <- validated_sample_df %>%
   count(city, locator, geocode_result) %>%
-  mutate(geocode_result = factor(geocode_result, levels = c("VALID", "GOOD", "AVERAGE", "BAD"),
+  mutate(geocode_result = factor(geocode_result,
+                                 levels = c("VALID", "GOOD", "AVERAGE", "BAD"),
                                  labels = c("VALID", "GOOD (< 350m)", "AVERAGE (< 1km)", "BAD")))
 
 fct_city <- validated_count_by_city %>%
-  filter(geocode_result == "VALID", locator == "streetmap_complete") %>%
+  filter(geocode_result == "VALID", locator == "google_maps") %>%
   arrange(n)
 
 
 validated_count_by_city %>%
+  filter(locator %in% c("streetmap_complete", "galileo", "google_maps")) %>%
+  mutate(locator = factor(locator, levels = c("google_maps", "streetmap_complete", "galileo"))) %>%
   mutate(city = factor(city, levels = fct_city$city)) %>%
+  mutate(geocode_result = fct_rev(geocode_result)) %>%
+  filter(locator %in% c("streetmap_complete", "galileo", "google_maps")) %>%
   # filter(geocode_result == "BAD") %>%
   ggplot() +
-  geom_col(aes(x=city, y = n, fill = locator, group = locator), position = "dodge") +
+  geom_col(aes(x=city, y = n, fill = geocode_result), position = "stack") +
   coord_flip() +
-  scale_fill_brewer(palette = "Set1") +
-  facet_wrap(~geocode_result)
+  scale_fill_brewer(palette = "Spectral", direction = 1, guide = guide_legend(reverse = FALSE)) +
+  scale_y_continuous(breaks = seq(0, 1000, 100), minor_breaks = seq(0, 1000, 50)) +
+  facet_wrap(~locator) +
+  theme(legend.position = "bottom")
+
 
 # By Locator
 validated_count_by_locator <- validated_sample_df %>%
